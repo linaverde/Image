ae25930d17d0c9f7dfec2cd8ba95de28d8a1120b
DieNice <DailyPlanning@bk.ru>
2021-04-01 14:10:18 +1000

    clearned docs

--- ParserLogs/filereader.py
+++
@@ -1,7 +0,0 @@
-class FileReader:
-    '''class for reading nginx or apache log from text files'''
-
-    def readlogs(self, filepath):
-        '''read logs from txt file'''
-        f = open(filepath)
-        return f.read().split('\n')
